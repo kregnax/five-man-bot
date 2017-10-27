8de645d19d3e65edc6acd894c278c95d75e337b9
Alan Kregness <Alan.Kregness@toro.com>
2017-10-27 13:05:17 -0500

    new json and environ testing

--- fiveman.py
+++ fiveman.py
@@ -1,16 +1,18 @@
-import discord
 import asyncio
-from bs4 import BeautifulSoup
-import urllib.request
+import json
+import os
 import random
 import re
 import sys
-import os
-import json
-import json_loader
-import fetch
-import requests
 import time
+import urllib.request
+
+import discord
+import requests
+from bs4 import BeautifulSoup
+
+import fetch
+import json_loader
 from hots_build_builder import BuildBuilder
 
 client = discord.Client()
@@ -22,19 +24,22 @@ build_builder = BuildBuilder()
 
 KEY = keys['five-man']
 
+
 @client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
+    print(os.environ.get('JAWSDB_URL'))
     print('--------')
+
 
 @client.event
 async def on_message(message):
     #TODO: use message.attachments to save images with text command
     if(message.content.startswith("$test")):
         if('lost vikings' in heroes_json['tlv']['alias']['misc']):
-            await client.send_message(message.channel,heroes_json['tlv']['alias']['hots-cntr'])
+            await client.send_message(message.channel, heroes_json['tlv']['alias']['hots-cntr'])
     if(message.content.startswith("!build")):
         hero = message.content.split()[1]
         print(hero)
@@ -48,17 +53,18 @@ async def on_message(message):
         if(str(message.author) == "kregnax#2710"):
             cmd_in = message.content.split()
             if(cmd_in[0] == "!addtxtcmd"):
-                text_commands[cmd_in[1]] = ''.join(w+' ' for w in cmd_in[2:]).strip()
+                text_commands[cmd_in[1]] = ''.join(
+                    w + ' ' for w in cmd_in[2:]).strip()
                 temp_json = json.dumps(text_commands)
                 temp_json = json.loads(temp_json)
-                with open('text_commands.json','w') as f:
-                    json.dump(temp_json, f,indent=4)
+                with open('text_commands.json', 'w') as f:
+                    json.dump(temp_json, f, indent=4)
             else:
                 await client.send_message(message.channel, "Unrecognized command: {}".format(cmd_in[0]))
         else:
             await client.send_message(message.channel, "You don't have access, pleb.")
     if(message.content == "?text"):
-        commands = 'Available text commands:\n';
+        commands = 'Available text commands:\n'
         for k, v in text_commands.items():
             commands += "!{}\n".format(k)
         await client.send_message(message.channel, commands)
@@ -80,6 +86,6 @@ async def on_message(message):
         await client.send_message(message.channel, counters)
     if(message.content.startswith('!patchnotes')):
         patch_notes_link = fetch.get_latest_patch_notes()
-        patch_notes_link = '3 most recent patches:\n'+patch_notes_link
+        patch_notes_link = '3 most recent patches:\n' + patch_notes_link
         await client.send_message(message.channel, patch_notes_link)
 client.run(KEY)
