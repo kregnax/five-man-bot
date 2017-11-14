febb61948066a53fca3e35fd00153c6300613a8f
Alan Kregness <Alan.Kregness@toro.com>
2017-11-14 16:33:28 -0600

    Testing lookup by level

--- FiveMan/hots_build_builder.py
+++ FiveMan/hots_build_builder.py
@@ -3,6 +3,9 @@ import json_loader
 import fetch
 
 class BuildBuilder(object):
+
+    VALID_LEVELS = [1, 4, 7, 10, 13, 16, 20]
+
     def __init__(self):
         self.heroes_json = json_loader.get_json("heroes.json")
     #TODO: break this out into smaller functions so selection of
@@ -13,9 +16,15 @@ class BuildBuilder(object):
         if '/' in request:
             sliced = request.split('/')
             hero = sliced[0]
-            tier = sliced[1]
-
-        return 'Could not find info for your request: ' + request
+            affix = sliced[1]
+            try:
+                level = int(affix)
+                return self.get_talents_for_level(hero, level)
+            except:
+                return 'Lookup by hero/talent not implemented'
+        else:
+            return self.get_talent(request)
+        return 'Not a valid request: ' + request
 
     def get_builds_for_hero(self, hero):
         if(hero not in self.heroes_json):
@@ -49,12 +58,22 @@ class BuildBuilder(object):
                 if t["name"].lower() == talent:
                     hero = hero[0].upper() + hero[1:]
                     found_talent += '__{}__: {} - {}\n\n'.format(hero,t["name"],t["description"])
+        if found_talent == '':
+            return 'Could not find info for talent __{}__'.format(talent)
         return found_talent
 
-    #def get_talent_for_tier(self, hero, tier):
-
-
-
+    def get_talents_for_level(self, hero, level):
+        if level not in self.VALID_LEVELS:
+            return level + ' is not a valid level'
+        true_name = fetch.get_hero_name(hero)
+        if true_name == 'Not a valid hero':
+            return true_name
+        talent_list = ''
+        tier = self.VALID_LEVELS.index(level) + 1        
+        for t in self.heroes_json[true_name]['talents']:
+            if tier == t['tier']:
+                talent_list += '__{}__: {}\n\n'.format(t['name'],t['description'])
+        return talent_list
 
     #TODO: get talents by name, maybe use string comparison with 85%
     #       threshhold of name to return relevant talents (see reddit bot)
